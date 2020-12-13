d59250f31149eafbf87837b0803e925e339f3e1a
Brian Fraser <bfraser@frasertechnicalsolutions.com>
2020-12-13 09:37:52 -0500

    Added prettier code formatting

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,98 +1,100 @@
-import * as React from 'react';
-import { IPerson } from '../models/IPerson';
-import { PersonCard } from './PersonCard';
-import styles from './SearchResults.module.scss';
+import * as React from "react";
+import { IPerson } from "../models/IPerson";
+import { PersonCard } from "./PersonCard";
+import styles from "./SearchResults.module.scss";
 
-export interface ISearchResultsProps {
- 
-}
+export interface ISearchResultsProps {}
 
-const SearchResults : React.FC<ISearchResultsProps> = (props: ISearchResultsProps) => {
+const SearchResults: React.FC<ISearchResultsProps> = (
+    props: ISearchResultsProps
+) => {
+    let peopleResults: IPerson[] = [];
 
-  let peopleResults: IPerson[] = [];
+    peopleResults.push({
+        id: "1",
+        name: "name",
+        extension: "ext",
+        title: "title",
+        assistantName: "rate",
+        assistantExtension: "a_ext",
+        photoUrl: "photo url",
+        office: "office"
+    });
 
-  peopleResults.push({
-    id: '1',
-    name: 'name', 
-    extension: 'ext', 
-    title: 'title', 
-    assistantName: 'rate', 
-    assistantExtension: 'a_ext',
-    photoUrl: 'photo url', 
-    office: 'office'
-  });
+    peopleResults.push({
+        id: "1",
+        name: "name",
+        extension: "ext",
+        title: "title",
+        assistantName: "rate",
+        assistantExtension: "a_ext",
+        photoUrl: "photo url",
+        office: "office"
+    });
 
-  peopleResults.push({
-    id: '1',
-    name: 'name', 
-    extension: 'ext', 
-    title: 'title', 
-    assistantName: 'rate', 
-    assistantExtension: 'a_ext',
-    photoUrl: 'photo url', 
-    office: 'office'
-  });
+    peopleResults.push({
+        id: "1",
+        name: "name",
+        extension: "ext",
+        title: "title",
+        assistantName: "rate",
+        assistantExtension: "a_ext",
+        photoUrl: "photo url",
+        office: "office"
+    });
 
-  peopleResults.push({
-    id: '1',
-    name: 'name', 
-    extension: 'ext', 
-    title: 'title', 
-    assistantName: 'rate', 
-    assistantExtension: 'a_ext',
-    photoUrl: 'photo url', 
-    office: 'office'
-  });
+    peopleResults.push({
+        id: "1",
+        name: "name",
+        extension: "ext",
+        title: "title",
+        assistantName: "rate",
+        assistantExtension: "a_ext",
+        photoUrl: "photo url",
+        office: "office"
+    });
 
-  peopleResults.push({
-    id: '1',
-    name: 'name', 
-    extension: 'ext', 
-    title: 'title', 
-    assistantName: 'rate', 
-    assistantExtension: 'a_ext',
-    photoUrl: 'photo url', 
-    office: 'office'
-  });
+    peopleResults.push({
+        id: "1",
+        name: "name",
+        extension: "ext",
+        title: "title",
+        assistantName: "rate",
+        assistantExtension: "a_ext",
+        photoUrl: "photo url",
+        office: "office"
+    });
 
- peopleResults.push({
-    id: '1',
-    name: 'name', 
-    extension: 'ext', 
-    title: 'title', 
-    assistantName: 'rate', 
-    assistantExtension: 'a_ext',
-    photoUrl: 'photo url', 
-    office: 'office'
-  });
-
-  return (
-    <div className={styles.searchResultsContainer}>
-      <div>
-        <div className={styles.resultsHeader}>
-          <div className={styles.headerContent}>
-            <span className={styles.contentCategory}>People Directory</span>
-            <label className={styles.contentTypeLabel}>People</label>
-            <select>
-              <option value="1">People</option>
-            </select>
-          </div>
+    return (
+        <div className={styles.searchResultsContainer}>
+            <div>
+                <div className={styles.resultsHeader}>
+                    <div className={styles.headerContent}>
+                        <span className={styles.contentCategory}>
+                            People Directory
+                        </span>
+                        <label className={styles.contentTypeLabel}>
+                            People
+                        </label>
+                        <select>
+                            <option value="1">People</option>
+                        </select>
+                    </div>
+                </div>
+                <div className={styles.resultsContainer}>
+                    <div className={styles.mainResultPanel}>
+                        <div className={styles.innerContainer}>
+                            {peopleResults.map((person: IPerson) => {
+                                return <PersonCard person={person} />;
+                            })}
+                        </div>
+                    </div>
+                    <div className={styles.resultNavPanel}>
+                        Results nav here
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className={styles.resultsContainer}>
-        <div className={styles.mainResultPanel}>
-          <div className={styles.innerContainer}>
-            {
-              peopleResults.map((person: IPerson) => {
-                return (
-                  <PersonCard person={person} />
-                );
-              })
-            }
-          </div>
-        </div>
-        <div className={styles.resultNavPanel}>Results nav here</div>
-        </div>
-        </div> 
-    </div>
-);};
+    );
+};
 export default SearchResults;
