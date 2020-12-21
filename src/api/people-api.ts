a198697218f649b657feaf8e7372615e855eb451
Brian Fraser <bfraiser@nge.com>
2020-12-21 09:36:32 -0500

    formatting people api

--- src/api/people-api.ts
+++ src/api/people-api.ts
@@ -2,28 +2,28 @@
 import { AadHttpClient, HttpClientResponse, IHttpClientOptions } from '@microsoft/sp-http';
 import { ApplicationCustomizerContext } from '@microsoft/sp-application-base';
 import { IPerson } from '../models/IPerson';
-import {config} from '../config';
+import { config } from '../config';
 
 export const searchPeople = async (searchTerm: string, context: ApplicationCustomizerContext): Promise<IPerson[]> => {
-  
+
   return context.aadHttpClientFactory.getClient(config.apiClientId).then((aadClient: AadHttpClient) => {
     const options: IHttpClientOptions = {
-      headers:{
-          'content-type': 'application/json',
-          'accept': 'application/json'
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
       },
       body: null
-  };
+    };
 
-  const uri = `${config.baseApiEndpoint}/api/people/directory`;
+    const uri = `${config.baseApiEndpoint}/api/people/directory`;
 
-  return aadClient.post(uri, AadHttpClient.configurations.v1, options);
+    return aadClient.post(uri, AadHttpClient.configurations.v1, options);
   }).then(async (response: HttpClientResponse): Promise<IPerson[]> => {
     const data = await response.json();
 
-    if (response.ok){
+    if (response.ok) {
       return data;
-    } else{
+    } else {
       return Promise.reject(data);
     }
   });
