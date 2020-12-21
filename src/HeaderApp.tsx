7203b835346cd49013a833e7e3cb0e05b286e79b
Brian Fraser <bfraiser@nge.com>
2020-12-21 10:01:16 -0500

    upgrade react-query to v3

--- src/HeaderApp.tsx
+++ src/HeaderApp.tsx
@@ -1,25 +1,39 @@
-import * as React from 'react';
-import { ApplicationCustomizerContext } from '@microsoft/sp-application-base';
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ApplicationCustomizerContext } from "@microsoft/sp-application-base";
 
-import GuidedSearch from './components/GuidedSearch';
+import GuidedSearch from "./components/GuidedSearch";
 
-import styles from './HeaderApp.module.scss';
+import styles from "./HeaderApp.module.scss";
 
-export interface IHeaderAppProps{
-  context: ApplicationCustomizerContext;
+export interface IHeaderAppProps {
+    context: ApplicationCustomizerContext;
 }
 
-export interface IHeaderAppState{
+export interface IHeaderAppState {}
 
-}
+const queryClient = new QueryClient({
+    defaultOptions: {}
+});
+export default class HeaderApp extends React.Component<
+    IHeaderAppProps,
+    IHeaderAppState
+> {
+    constructor(props: IHeaderAppProps) {
+        super(props);
+        this.state = {};
+    }
 
-export default class HeaderApp extends React.Component<IHeaderAppProps, IHeaderAppState> {
-  constructor(props: IHeaderAppProps){
-    super(props);
-    this.state = {};
-  }
-
-  public render(): React.ReactElement<IHeaderAppProps> {
-  return <div className={styles.headerContainer}><GuidedSearch initialText='search people, clients, matters, and documents...' context={this.props.context} /></div>;
-  }
+    public render(): React.ReactElement<IHeaderAppProps> {
+        return (
+            <div className={styles.headerContainer}>
+                <QueryClientProvider client={queryClient}>
+                    <GuidedSearch
+                        initialText="search people, clients, matters, and documents..."
+                        context={this.props.context}
+                    />
+                </QueryClientProvider>
+            </div>
+        );
+    }
 }
--- src/hooks/usePeopleSearch.ts
+++ src/hooks/usePeopleSearch.ts
@@ -1,12 +1,8 @@
 import { ApplicationCustomizerContext } from '@microsoft/sp-application-base';
-import {QueryResult, useQuery} from 'react-query';
-import {searchPeople} from '../api/people-api';
+import { QueryObserverResult, useQuery } from 'react-query';
+import { searchPeople } from '../api/people-api';
 import { IPerson } from '../models/IPerson';
 
-const executePeopleSearch = async (searchTerm: string, context: ApplicationCustomizerContext) => {
-  return searchPeople(searchTerm, context);
-};
-
-export default function usePeopleSearch(searchTerm: string, context: ApplicationCustomizerContext): QueryResult<IPerson[], Error> {
-  return useQuery(['search', searchTerm], () => executePeopleSearch(searchTerm, context), {staleTime: Infinity, retry: 1});
+export default function usePeopleSearch(searchTerm: string, context: ApplicationCustomizerContext) : QueryObserverResult<IPerson[], Error> {
+  return useQuery<IPerson[], Error>(['search', searchTerm], () => searchPeople(searchTerm, context), { staleTime: Infinity, retry: 1 });
 }
