153a042ec79b63fb74f11671e0368119c95996bf
Brian Fraser <bfraiser@nge.com>
2020-12-21 09:36:11 -0500

    created config for common settings - like api key and api endpoint

--- src/api/people-api.ts
+++ src/api/people-api.ts
@@ -2,10 +2,11 @@
 import { AadHttpClient, HttpClientResponse, IHttpClientOptions } from '@microsoft/sp-http';
 import { ApplicationCustomizerContext } from '@microsoft/sp-application-base';
 import { IPerson } from '../models/IPerson';
+import {config} from '../config';
 
 export const searchPeople = async (searchTerm: string, context: ApplicationCustomizerContext): Promise<IPerson[]> => {
   
-  return context.aadHttpClientFactory.getClient('68183d97-02fe-4d25-a830-fc87890bbba5').then((aadClient: AadHttpClient) => {
+  return context.aadHttpClientFactory.getClient(config.apiClientId).then((aadClient: AadHttpClient) => {
     const options: IHttpClientOptions = {
       headers:{
           'content-type': 'application/json',
@@ -14,7 +15,7 @@ export const searchPeople = async (searchTerm: string, context: ApplicationCusto
       body: null
   };
 
-  const uri = 'https://ngeaccess.azurewebsites.net/api/people/directory';
+  const uri = `${config.baseApiEndpoint}/api/people/directory`;
 
   return aadClient.post(uri, AadHttpClient.configurations.v1, options);
   }).then(async (response: HttpClientResponse): Promise<IPerson[]> => {
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  apiClientId: '68183d97-02fe-4d25-a830-fc87890bbba5',
+  baseApiEndpoint: 'https://ngeaccess.azurewebsites.net'
+};
