7511c432ce3c91fcdae75ec0de1975315e10ada7
Brian Fraser <bfraiser@nge.com>
2020-12-23 14:37:33 -0500

    Updated people search query settings

--- src/hooks/usePeopleSearch.ts
+++ src/hooks/usePeopleSearch.ts
@@ -4,5 +4,5 @@ import { searchPeople } from '../api/people-api';
 import { IPerson } from '../models/IPerson';
 
 export const usePeopleSearch = (searchTerm: string, context: ApplicationCustomizerContext): QueryObserverResult<IPerson[], Error> => {
-  return useQuery<IPerson[], Error>(['search', searchTerm], () => searchPeople(searchTerm, context), { staleTime: Infinity, retry: 1 });
+  return useQuery<IPerson[], Error>(['person-search', searchTerm], () => searchPeople(searchTerm, context), { staleTime: 60000, retry: 2 });
 };
