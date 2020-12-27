fb22c5f3304430edf8e780ba8ca6c342238cdbf4
Brian Fraser <bfraiser@nge.com>
2020-12-26 21:58:19 -0500

    created client and matter models

---
+++ src/models/IClient.ts
@@ -0,0 +1,9 @@
+export interface IClient {
+  clientNumber: string;
+  clientName: string;
+  address1: string;
+  address2: string;
+  shortName: string;
+  status: string;
+  openDate: string;
+}
---
+++ src/models/IMatter.ts
@@ -0,0 +1,10 @@
+export interface IMatter {
+  matterNumber: string;
+  matterName: string;
+  matterDesc: string;
+  department: string;
+  practice: string;
+  openDate: Date;
+  status: string;
+  client: string;
+}
