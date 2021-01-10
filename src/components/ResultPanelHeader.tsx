fc8eabf8f4cfc8dd73614dd3760469ea4aaac689
Brian Fraser <bfraiser@nge.com>
2021-01-10 15:31:20 -0500

    Removed drop down from results header

--- src/components/ResultPanelHeader.tsx
+++ src/components/ResultPanelHeader.tsx
@@ -8,15 +8,6 @@ export const ResultPanelHeader: React.FC<
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <div className={styles.title}>
-                    <span>People Directory</span>
-                </div>
-                <div className={styles.dropdownContainer}>
-                    <label>Default Search</label>
-                    <select>
-                        <option value="1">People</option>
-                    </select>
-                </div>
             </div>
         </div>
     );
