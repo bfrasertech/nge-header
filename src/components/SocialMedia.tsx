8c85a5642df8c478907d75c94efd9dcb7c113e5c
Brian Fraser <bfraiser@nge.com>
2021-01-13 17:28:20 -0500

    Updated social media

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 
 import styles from "./SocialMedia.module.scss";
 
-const ngeLogo: any = require('../images/nge-logo.png');
-const linkedin: any = require('../images/linkedin.png');
-const twitter: any = require('../images/twitter.png');
-const instagram: any = require('../images/instagram.png');
+const ngeLogo: any = require("../images/nge-logo.png");
+const linkedin: any = require("../images/linkedin.png");
+const twitter: any = require("../images/twitter.png");
+const instagram: any = require("../images/instagram.png");
 
 export const SocialMedia: React.FC<
     Record<string, unknown>
@@ -13,10 +13,22 @@ export const SocialMedia: React.FC<
     return (
         <div className={styles.container}>
             <div className={styles.innerContainer}>
-                <span className={styles.logo}><img src={ngeLogo} alt=""/></span>
-                <span className={styles.linkedin}><img src={linkedin} alt=""/></span>
-                <span className={styles.twitter}><img src={twitter} alt=""/></span>
-                <span className={styles.instagram}><img src={instagram} alt=""/></span>
+                <span className={styles.logo}>
+                    <a href="https://www.nge.com" target="_blank">
+                        <img src={ngeLogo} alt="" />
+                    </a>
+                </span>
+                <span className={styles.linkedin}>
+                    <a href="#">
+                        <img src={linkedin} alt="" />
+                    </a>
+                </span>
+                <span className={styles.twitter}>
+                    <a href="#"><img src={twitter} alt="" /></a>
+                </span>
+                <span className={styles.instagram}>
+                    <a href="#"><img src={instagram} alt="" /></a>
+                </span>
             </div>
         </div>
     );
